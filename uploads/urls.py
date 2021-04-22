46b5c18cbc6a0271c36240ee9f70b3cbab97f340
andruhus <aaademed@gmail.com>
2021-04-22 11:54:25 +0300

    Work test

--- uploads/urls.py
+++ uploads/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from uploads.core import views
+from .core import views
 
 
 urlpatterns = [
